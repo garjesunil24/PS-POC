016873cb752e07b5f82f7c17a729d2103f4c6b1f
garjesunil24 <garjesunil24@gmail.com>
2025-07-01 15:28:28 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,71 @@
+# Security Group for Load Balancer
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP to ALB"
+  vpc_id      = aws_vpc.ps_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "tg" {
+  name     = "nginx-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ps_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+# Attach EC2 instance to Target Group
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.nginx_instance.id
+  port             = 80
+}
+
+# Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "nginx-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "nginx-alb"
+  }
+}
+
+# Listener for ALB
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
